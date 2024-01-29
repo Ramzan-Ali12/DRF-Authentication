6caef2c24b89fe15f26c964d5ea73188d7195afb
Ramzan-Ali12 <m.ramzan.iub.edu@gmail.com>
2024-01-29 11:58:59 +0500

    auth-api-updated

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,14 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
-from django.core.validators import EmailValidator
 from django.core.exceptions import ValidationError
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'read_only': True}}
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def validate_password(self, value):
         try:
@@ -19,9 +18,9 @@ class UserSerializer(serializers.ModelSerializer):
 
     def validate_email(self, value):
         if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError('Email must be Unique!')
+            raise serializers.ValidationError('Email must be unique!')
         return value
-      
+
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
